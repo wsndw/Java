0cb256586ec2a6343333746877a29bdd85377b81
wsndw <460858975@qq.com>
2020-09-11 14:33:53 +0800

    JVM

---
+++ Java/JVM/jvm-demo/chapter04/src/main/java/cn/cq/jvm/PCRegisterTest.java
@@ -0,0 +1,16 @@
+package cn.cq.jvm;
+
+/**
+ * PCRegisterTest <br>
+ *
+ * @author CQ <br>
+ * @version 1.0 <br>
+ * @date 2020-09-11 11:31 <br>
+ */
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 10;
+        int k = i + j;
+    }
+}
